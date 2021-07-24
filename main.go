29bd32dfeb7831407e3448738ca0795c4fb7a247
ziaurgithub <ziaur252729@gmail.com>
2021-07-24 10:20:31 +0600

    number9

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+import "fmt"
+func main(){
+
+
+//var isfound bool
+//var name string
+//var age int
+//var result float32
+
+/*fmt.Println(name)
+fmt.Println(age)
+fmt.Println(result)
+fmt.Println(isfound)
+fmt.Println(true && true)
+fmt.Println(true && false)
+fmt.Println(true || true)
+fmt.Println(true || false)
+fmt.Println(!true)
+*/
+var chr rune
+chr = 'a'
+fmt.Println(chr)
+fmt.Printf("%c", chr)
+}
+
